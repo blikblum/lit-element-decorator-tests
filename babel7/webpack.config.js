4efbf2af56fd9acbba5dab08ec9326a4e9e4d240
Luiz Américo <luizamericop@gmail.com>
2019-01-04 11:26:59 -0300

    Add babel 7 spec build

--- babel7/webpack.config.js
+++ babel7/webpack.config.js
@@ -1,48 +1,94 @@
 const path = require('path')
 
-module.exports = {
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        include: [path.resolve('src')],
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            'presets': [
-              [
-                '@babel/preset-env',
-                {
-                  'targets': {
-                    'browsers': [
-                      'chrome 65'
-                    ]
-                  },
-                  'modules': false
-                }
-              ]
-            ],
-            'plugins': [
-              [
-                '@babel/plugin-proposal-decorators',
-                {
-                  'legacy': true
-                }
-              ],
-              [
-                '@babel/plugin-proposal-class-properties',
-                {
-                  'loose': true
-                }
-              ]
-            ]
-          }
-        }]
-      }
-    ]
+const babelPresets = [
+  [
+    '@babel/preset-env',
+    {
+      'targets': {
+        'browsers': [
+          'chrome 65'
+        ]
+      },
+      'modules': false
+    }
+  ]
+]
+
+const legacyPluginsConfig = [
+  [
+    '@babel/plugin-proposal-decorators',
+    {
+      'legacy': true
+    }
+  ],
+  [
+    '@babel/plugin-proposal-class-properties',
+    {
+      'loose': true
+    }
+  ]
+]
+
+const specPluginsConfig = [
+  [
+    '@babel/plugin-proposal-decorators',
+    {
+      legacy: false,
+      decoratorsBeforeExport: true
+    }
+  ],
+  '@babel/plugin-proposal-class-properties'
+]
+
+module.exports = [
+  {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      filename: 'main-legacy.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          include: [path.resolve('src')],
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: babelPresets,
+              plugins: legacyPluginsConfig
+            }
+          }]
+        }
+      ]
+    },
+    optimization: {
+      minimize: false
+    }
   },
-  optimization: {
-    minimize: false
+  {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      filename: 'main-spec.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          include: [path.resolve('src')],
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: babelPresets,
+              plugins: specPluginsConfig
+            }
+          }]
+        }
+      ]
+    },
+    optimization: {
+      minimize: false
+    }
   }
-}
+]
